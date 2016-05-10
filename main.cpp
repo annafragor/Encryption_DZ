45ea71cdc34bcb402a46794d4861c968308d0ec8
moskanka <moskanka@yandex.ru>
2016-05-10 08:47:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,90 +9,17 @@
 #include <conio.h>
 using namespace std;
 
+string alphabet = "abcdefghigklmnopqrstuvwxyz|.?";
 int fromChar(char c)
-{
-	int result;
-	switch (c)
-	{
-	case 'a': result = 0; break;
-	case 'b': result = 1; break;
-	case 'c': result = 2; break;
-	case 'd': result = 3; break;
-	case 'e': result = 4; break;
-	case 'f': result = 5; break;
-	case 'g': result = 6; break;
-	case 'h': result = 7; break;
-	case 'i': result = 8; break;
-	case 'j': result = 9; break;
-	case 'k': result = 10; break;
-	case 'l': result = 11; break;
-	case 'm': result = 12; break;
-	case 'n': result = 13; break;
-	case 'o': result = 14; break;
-	case 'p': result = 15; break;
-	case 'q': result = 16; break;
-	case 'r': result = 17; break;
-	case 's': result = 18; break;
-	case 't': result = 19; break;
-	case 'u': result = 20; break;
-	case 'v': result = 21; break;
-	case 'w': result = 22; break;
-	case 'x': result = 23; break;
-	case 'y': result = 24; break;
-	case 'z': result = 25; break;
-	case '|': result = 26; break;
-	case '.': result = 27; break;
-	case '?': result = 28; break;
-	default:
-		throw out_of_range("wrong input symbol");
-	}
-	return result;
-}
+{	return alphabet.find(c);	}
+char fromInt(int i)
+{	return alphabet[i];	}
 vector<int> translate(string str)
 {
 	vector<int> result;
 	for (unsigned int i = 0; i < str.length(); i++)
 	{
 		result.push_back(fromChar(str[i]));
-	}
-	return result;
-}
-char fromInt(int i)
-{
-	char result;
-	switch (i)
-	{
-	case 0: result = 'a'; break;
-	case 1: result = 'b'; break;
-	case 2: result = 'c'; break;
-	case 3: result = 'd'; break;
-	case 4: result = 'e'; break;
-	case 5: result = 'f'; break;
-	case 6: result = 'g'; break;
-	case 7: result = 'h'; break;
-	case 8: result = 'i'; break;
-	case 9: result = 'j'; break;
-	case 10: result = 'k'; break;
-	case 11: result = 'l'; break;
-	case 12: result = 'm'; break;
-	case 13: result = 'n'; break;
-	case 14: result = 'o'; break;
-	case 15: result = 'p'; break;
-	case 16: result = 'q'; break;
-	case 17: result = 'r'; break;
-	case 18: result = 's'; break;
-	case 19: result = 't'; break;
-	case 20: result = 'u'; break;
-	case 21: result = 'v'; break;
-	case 22: result = 'w'; break;
-	case 23: result = 'x'; break;
-	case 24: result = 'y'; break;
-	case 25: result = 'z'; break;
-	case 26: result = '|'; break;
-	case 27: result = '.'; break;
-	case 28: result = '?'; break;
-	default:
-		throw out_of_range("wrong input symbol!");
 	}
 	return result;
 }
